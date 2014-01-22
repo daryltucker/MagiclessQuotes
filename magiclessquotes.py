1df4541d2e32a257ff1c5bc8d0c2b098930fdbb8
Daryl Tucker <daryl@daryltucker.com>
2014-01-22 11:29:17 -0600

    RC1

---
+++ magiclessquotes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Daryl Tucker"
+
+import sublime, sublime_plugin
+
+class RemoveMagicFromMagic(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        replacements = [
+            [u'[’‘]{1}',u'\''],
+            [u'[“”]{1}',u'"'],
+            [u'[…]{1}',u'...'],
+            [u'[—]{1}',u'---'],
+            [u'[–]{1}',u'--'],
+            [u'[•]{1}',u'*'],
+        ]
+        edit = view.begin_edit()
+        for replacement in replacements:
+            x = view.find_all(replacement[0])
+            for position in x:
+                view.replace(edit, position, replacement[1])
+        view.end_edit(edit)
