c0f0e001366f950ea2449bebc9c87580a7653d30
mynameiskyle <kyle@collegefrog.com>
2014-01-24 09:57:28 -0600

    Adding spaces

--- magiclessquotes.py
+++ magiclessquotes.py
@@ -14,7 +14,7 @@ class RemoveMagicFromMagic(sublime_plugin.EventListener):
             [u'[—]{1}',u'---'],
             [u'[–]{1}',u'--'],
             [u'[•]{1}',u'*'],
-            [u'[&]{1}',u'&amp;'],
+            [u'[ & ]{1}',u' &amp; '],
         ]
         edit = view.begin_edit()
         for replacement in replacements:
