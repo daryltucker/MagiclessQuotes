00aaf9903693fbb45b20254820c20b73c9f7a928
Daryl Tucker <daryl@daryltucker.com>
2015-02-23 13:50:38 -0600

    Fixing Multi-Replace on Single Character Finds
    Fixes #4

--- magiclessquotes.py
+++ magiclessquotes.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sublime
+import sublime_plugin
+
 __author__ = "Daryl Tucker"
 
-import sublime, sublime_plugin
 
 class RemoveMagicFromMagicCommand(sublime_plugin.TextCommand):
     def run(self, edit, user_input=None):
         self.edit = edit
         replacements = [
-            [u'[’‘]{1}',u'\''],
-            [u'[“”]{1}',u'"'],
-            [u'[…]{1}',u'...'],
-            [u'[—]{1}',u'---'],
-            [u'[–]{1}',u'--'],
-            [u'[•]{1}',u'*'],
+            [u'[’‘]{1}', u'\''],
+            [u'[“”]{1}', u'"'],
+            [u'[…]{1}', u'...'],
+            [u'[—]{1}', u'---'],
+            [u'[–]{1}', u'--'],
+            [u'[•]{1}', u'*'],
             #[u' & ',u' &amp; '],
         ]
 
         for replacement in replacements:
             x = self.view.find_all(replacement[0])
+            dif = 1 - len(replacement[1])
+            m = 0
             for position in x:
-                self.view.replace(edit, position, replacement[1])
-        #self.view.end_edit(edit)
+                difx = m * dif
+                p = list(position)
+                pos = sublime.Region(p[0] - difx, p[1] - difx)
+                self.view.replace(edit, pos, replacement[1])
+                m += 1
+        self.view.end_edit(edit)
+
 
 class RunMagic(sublime_plugin.EventListener):
     def on_pre_save(self, view):
